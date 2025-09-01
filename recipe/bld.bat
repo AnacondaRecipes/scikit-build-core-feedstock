526ebb0c54f745bc561f3ac57a36b16643dfed5a
TS-ANC <208963744+TS-ANC@users.noreply.github.com>
2025-09-01 12:10:11 +0200

    scikit-build-core 0.11.6  (#16)
    * version 0.11.6
    
    * update python version
    
    * update deps
    
    * patch tests
    
    * update test dependencies
    
    * adjust test pinnings
    
    * restore test
    
    * remove activation scripts
    
    * don't set cmake generator on windows
    
    * restore test
    
    * remove tailing slashes
    
    * cleanup patching
    
    * remove cmake license file

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,15 +1,1 @@
 python -m pip install . --no-deps --no-build-isolation -vv
-
-set ACTIVATE_DIR=%PREFIX%\etc\conda\activate.d
-set DEACTIVATE_DIR=%PREFIX%\etc\conda\deactivate.d
-if not exist "%ACTIVATE_DIR%" mkdir %ACTIVATE_DIR%
-if errorlevel 1 exit 1
-if not exist "%DEACTIVATE_DIR%" mkdir %DEACTIVATE_DIR%
-if errorlevel 1 exit 1
-
-copy %RECIPE_DIR%\scripts\activate.bat %ACTIVATE_DIR%\%PROJECT_NAME%-activate.bat
-if errorlevel 1 exit 1
-
-copy %RECIPE_DIR%\scripts\deactivate.bat %DEACTIVATE_DIR%\%PROJECT_NAME%-deactivate.bat
-if errorlevel 1 exit 1
-
